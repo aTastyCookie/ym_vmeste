8dc4940085f46871815176d2cc2ce487f67ca09d
DigitalCommunity <ant1freezeca@gmail.com>
2014-10-25 14:58:01 +0400

    zip all dir

--- proma/config/config.php
+++
@@ -1,8 +0,0 @@
-<?php
-define('DB_HOST', 'localhost');
-define('DB_NAME', 'promotable');
-define('DB_USER', 'promouser');
-define('DB_PASSWORD', 'superpw');
-define('DB_TABLE_NAME', 'promo_requests');
-define('TIME_LIMIT', 60);
-?>
--- src/vmeste/src/Vmeste/SaasBundle/VmesteSaasBundle.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-namespace Vmeste\SaasBundle;
-
-use Symfony\Component\HttpKernel\Bundle\Bundle;
-
-class VmesteSaasBundle extends Bundle
-{
-}
