28b21aeaed4c495f9844c1670c4f540a467d8ad7
Andrey Glyatsevich <paction@bk.ru>
2014-11-30 22:20:12 +0300

    remove config from index

--- kassa/src/vmeste/web/kassa/config/config.php
+++
@@ -1,8 +0,0 @@
-<?php
-define('DB_HOST', '127.0.0.1');
-define('DB_NAME', 'kassa');
-define('DB_USER', 'root');
-define('DB_PASSWORD', 'differ3ent');
-define('DB_TABLE_NAME', 'promo_requests');
-define('TIME_LIMIT', 60);
-?>
